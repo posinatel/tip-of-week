5b30278e0115803e1048cdb6c48c4c96acf94f15
Carlos Augusto Lima Grispan <carlos.lima@inatel.br>
2018-04-27 11:48:59 -0300

    Improve proxy

--- tips/proxy.js
+++ tips/proxy.js
@@ -9,14 +9,14 @@ const target = {
 
 const target2 = {
   name: 'Leandro',
-  age: 26
+  nationality: 'Brazilian'
 };
 
 const handler = {
   get: (target, property) => {
-    try {
+    const message = 'No available data';
 
-      let message = 'No available data';
+    try {
 
       if (property === 'lastDog') {
         return target.dogs[target.dogs.length - 1];
@@ -27,12 +27,13 @@ const handler = {
       }
 
       if (property === 'age') {
-        message = target.name === 'Carlos' ? 'Older than it looks!' : 'Looks old...';
+        return target.name === 'Carlos' ? 'Older than it looks!' : 'Looks old...';
       }
 
       return target[property] || message;
-    } catch (err) {
-      return 'No available data';
+
+    } catch (error) {
+      return message;
     }
   },
 
@@ -43,43 +44,44 @@ const handler = {
     }
 
     target[property] = value;
-    console.log(`He actually looks ${value}`);
+    console.log(`He actually looks ${value}...`);
     return true;
   },
 
   deleteProperty: function (target, property) {
     if (property === 'name' && target[property] === 'Carlos') {
-      console.log('I don\'t wanna lose my id');
+      console.log('I don\'t wanna lose my identity');
       return false;
     }
     delete target[property];
+    return true;
   }
 
 };
 
 const carlos = new Proxy(target, handler);
 
-console.log('age:', carlos.age);
-console.log('nacionality:', carlos.nacionality);
-console.log('lastDog:', carlos.lastDog);
-console.log('firstDog:', carlos.firstDog);
+console.log('age:', carlos.age); // age: Older than it looks!
+console.log('nationality:', carlos.nationality); // nationality: No available data
+console.log('firstDog:', carlos.firstDog); // firstDog: Lessie
+console.log('lastDog:', carlos.lastDog); // lastDog: Pit
 
-carlos.age = 35;
+carlos.age = 35; // 35?? Are you kidding me?
 
-delete carlos.name;
-console.log(carlos.name);
+delete carlos.name; // I don't wanna lose my identity
+console.log('name: ', carlos.name); // Carlos
 
 console.log('---------------------------------:');
 
 const leandro = new Proxy(target2, handler);
 
-console.log('age:', leandro.age);
-console.log('nacionality:', leandro.nacionality);
-console.log('lastDog:', leandro.lastDog);
-console.log('firstDog:', leandro.firstDog);
+console.log('age:', leandro.age); // age: Looks old...
+console.log('nationality:', leandro.nationality);  // nationality: Brazilian
+console.log('firstDog:', leandro.firstDog); // firstDog: No available data
+console.log('lastDog:', leandro.lastDog); // lastDog: No available data
 
-leandro.age = 35;
+leandro.age = 35; // He actually looks 35...
 
 delete leandro.name;
-console.log(leandro.name);
+console.log('name: ', leandro.name); // name: No available data
 
